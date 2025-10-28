c192d2a9b93d71baca30eac8ab3d17a4efddb564
anirudhflutter <anirudhflutter@gmail.com>
2025-10-28 11:34:02 +0100

    updated static files direct

--- poker_lounge/settings.py
+++ poker_lounge/settings.py
@@ -163,7 +163,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Define STATIC_ROOT for collectstatic (used in production)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files (Uploaded by users)
 MEDIA_URL = '/media/'
